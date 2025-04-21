dfd5e6d4f066606ef96690811bc4b2cd40213d3b
MatteoGiambarresi <matteo.giambarresi@student.ehb.be>
2025-04-21 23:03:27 +0200

    add ProjectDetail component and update routing in App component

--- Main/src/App.jsx
+++ Main/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Projects from "./components/Projects";
+
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
-
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import ProjectDetail from "./components/ProjectDetail";
 
 export default function App() {
   return (
@@ -11,7 +13,9 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
       </Routes>
     </Router>
   );
