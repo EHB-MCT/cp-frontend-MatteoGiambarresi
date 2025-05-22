1e2d00530ab0fd0d3a26b8858567316aff4b38be
MatteoGiambarresi <matteo.giambarresi@student.ehb.be>
2025-05-22 16:00:01 +0200

    Added Scene4 component with animation

--- src/pages/Parallax.jsx
+++ src/pages/Parallax.jsx
@@ -4,6 +4,7 @@ import { ZoomIn } from "../components/ZoomIn";
 import Scene1 from "../components/Scene1";
 import Scene2 from "../components/Scene2";
 import Scene3 from "../components/Scene3";
+import Scene4 from "../components/Scene4";
 import { Parallax } from "@react-spring/parallax";
 import Functionalities from "../components/Functionalities";
 import Text1 from "../components/Text1";
@@ -19,14 +20,14 @@ export default function ParallaxW() {
     controls4,
     controls5,
     playSound,
-		refs: [ref1, ref2, ref3],
-		inViews: [inView1, inView2, inView3],
+		refs: [ref1, ref2, ref3,  ref4, ref5, ref6],
+		inViews: [inView1, inView2, inView3, inView4, inView5, inView6],
 	} = Functionalities();
 
 	return (
 		<div className="wrapper">
 			<ZoomIn controls={controls} />
-			<Parallax pages={7} style={{ top: "0", left: "0" }} className="animation">
+			<Parallax pages={9} style={{ top: "0", left: "0" }} className="animation">
 				<Scene1 />
 				<Text1 ref1={ref1} inView1={inView1} />
 				<Scene2 controls2={controls2} controls3={controls3} />
@@ -34,6 +35,7 @@ export default function ParallaxW() {
 				<Scene3 control4={controls4} controls5={controls5} />
         <Notes playSound={playSound} />
         <Text3 ref3={ref3} inView3={inView3} />
+        <Scene4 ref4={ref4} inView4={inView4} />
 			</Parallax>
 		</div>
 	);
